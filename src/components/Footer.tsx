6e43aa8e40045d7cc62d9106c695ee85e22068eb
vito m <vitomortalo@gmail.com>
2025-07-27 16:04:45 -0600

    changed the links in the footer to work

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,27 +3,6 @@ const Footer = () => {
     <footer className="w-full h-[33vh] bg-[image:var(--bg-main)] text-[var(--text)] ">
       <div className="max-w-6xl mx-auto px-8 py-10 grid grid-cols-1 sm:grid-cols-3 gap-8 text-left">
         <div>
-          <h2 className="text-2xl font-semibold mb-3 underline">Quick Links</h2>
-          <ul className="space-y-2">
-            <li>
-              <a href="#" className="hover:underline">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Visualizations
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                About
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div>
           <h2 className="text-2xl font-semibold mb-3 underline">My Info</h2>
           <p className="text-sm leading-relaxed">
             Created by Vito Mortalo
@@ -35,12 +14,12 @@ const Footer = () => {
         <div>
           <h2 className="text-2xl font-semibold mb-3 underline">GitHub</h2>
           <a
-            href="https://github.com/yourusername"
+            href="https://github.com/vito405/Sorting-Visualizer"
             target="_blank"
             rel="noopener noreferrer"
             className=" hover:underline"
           >
-            github.com/yourusername
+            github.com/vito405/Sorting-Visualizer
           </a>
         </div>
       </div>
